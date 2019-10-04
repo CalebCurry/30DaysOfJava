7bc4c192cfbed6823ec3ac4f828a7fdef1d76603
Caleb Curry <calebcurry@Calebs-MacBook-Pro.local>
2019-10-04 05:36:54 -0400

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {  
+		System.out.println("Enter your name");
+		
+		Scanner in = new Scanner(System.in);
+	    //String s = in.nextLine(); 
+	    
+
+	    int i = 0; //initialization
+	    
+	    do {
+	    	System.out.println("i is: " + i);
+	    	i++;
+	    } while(false);
+	    
+	    
+	    
+	    in.close();
+	}
+}
