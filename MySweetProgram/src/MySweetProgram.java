9cd0fd2dc89b0955ed9e7636ee5de2a32e4ad2a0
Caleb Curry <calebcurry@Calebs-MacBook-Pro.local>
2019-10-08 15:08:01 -0400

    Simple queueueeueeee:

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,24 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MySweetProgram {
 
 	public static void main(String[] args) throws FileNotFoundException {  
+		LinkedList<String> names = new LinkedList<String>();
+		names.add("Caleb");
+		names.add("Sue");
+		names.add("Sally");
 
-		Scanner in = new Scanner(new File("students.txt"));
-		
-		List<String> students = new ArrayList<String>();
-		
-		while(in.hasNextLine()) {
-			students.add(in.nextLine());
-		}
-		
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("name: " + students.get(i));
-		}
-		in.close();
+		System.out.println(names.remove());
+		System.out.println(names.remove());
+		System.out.println(names.remove());
 	}
+	
 }
