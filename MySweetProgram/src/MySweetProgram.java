1383674ac2b994a94ec01bba24412197a6a442fc
Caleb Curry <calebcurry@Calebs-MacBook-Pro.local>
2019-10-10 17:00:48 -0400

    iterators

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -2,22 +2,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MySweetProgram {
 
-	public static void main(String[] args) {  
-		
+	public static void main(String[] args){  	
 		LinkedList<String> names = new LinkedList<String>();
 		names.push("Caleb");
 		names.push("Sue");
 		names.push("Sally");
 		
-		System.out.println(names.pop());
-		System.out.println(names.removeFirst());
-		System.out.println(names.remove());
+		ListIterator<String> it = names.listIterator();
+		it.next();
+		it.next();
+		it.add("Susan");
+		
+		for(String s: names) {
+			System.out.println(s);
+		}
 		
 	}
 	
